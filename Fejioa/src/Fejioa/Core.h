5e260a9c14f3572fef1510494f7690f04cc5f21d
Parth <developmentprogrmming154@gmail.com>
2021-08-24 21:33:24 +0200

    Added Fejioa::Scope and Fejioa::Ref definitions

--- Fejioa/src/Fejioa/Core.h
+++ Fejioa/src/Fejioa/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #ifdef FJ_PLATFORM_WINDOWS
 	#ifdef FJ_DYNAMIC_LINK
 		#ifdef FJ_BUILD_DLL
@@ -29,3 +31,12 @@
 #define BIT(x) (1 << x)
 
 #define FJ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Fejioa
+{
+	template <typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template <typename T>
+	using Ref = std::shared_ptr<T>;
+}
