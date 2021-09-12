27e5968704b84fdb5af79c60ae6a6bfb65133d04
Parth <developmentprogrmming154@gmail.com>
2021-09-12 20:15:18 +0200

    Missing iterator wrappers in LayerStack.h

--- Fejioa/src/Fejioa/Core/Application.cpp
+++ Fejioa/src/Fejioa/Core/Application.cpp
@@ -60,9 +60,9 @@ namespace Fejioa
 		dispatcher.Dispatch<WindowCloseEvent>(FJ_BIND_EVENT_FN(Application::OnWindowClose));
 		dispatcher.Dispatch<WindowResizeEvent>(FJ_BIND_EVENT_FN(Application::OnWindowResize));
 
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); it++)
 		{
-			(*--it)->OnEvent(e);
+			(*it)->OnEvent(e);
 			if (e.Handled)
 				break;
 		}
