1b41cdb35850437e9bfae19fa63b87bef0465062
Parth <developmentprogrmming154@gmail.com>
2021-09-29 12:44:11 +0200

    Refactored Input

--- Feijoa/src/Feijoa/Core/Input.h
+++ Feijoa/src/Feijoa/Core/Input.h
@@ -9,29 +9,11 @@ namespace Feijoa
 {
 	class Input
 	{
-	protected:
-		Input() = default;
-
 	public:
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
-
-		static inline bool IsKeyPressed(KeyCode keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
-		static inline bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		static inline std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		static inline float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		static inline float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-
-		static Scope<Input> Create();
-
-	protected:
-		virtual bool IsKeyPressedImpl(KeyCode keycode) = 0;
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-
-	private:
-		static Scope<Input> s_Instance;
+		static bool IsKeyPressed(KeyCode keycode);
+		static bool IsMouseButtonPressed(MouseCode button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 }
--- Feijoa/src/Platform/Windows/WindowsInput.cpp
+++ Feijoa/src/Platform/Windows/WindowsInput.cpp
@@ -1,26 +1,27 @@
 #include "fjpch.h"
-#include "WindowsInput.h"
+#include "Feijoa/Core/Input.h"
 
 #include "Feijoa/Core/Application.h"
 #include <GLFW/glfw3.h>
 
+#ifdef FJ_PLATFORM_WINDOWS
 namespace Feijoa
 {
-	bool WindowsInput::IsKeyPressedImpl(KeyCode keycode)
+	bool Input::IsKeyPressed(KeyCode keycode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(keycode));
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button)
+	bool Input::IsMouseButtonPressed(MouseCode button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
 		return state == GLFW_PRESS;
 	}
 
-	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	std::pair<float, float> Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
@@ -29,16 +30,17 @@ namespace Feijoa
 		return std::make_pair((float)xpos, (float)ypos);
 	}
 
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
 		auto [x, y] = GetMousePosition();
 		return x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
 		auto [x, y] = GetMousePosition();
 		return y;
 	}
 
 }
+#endif
