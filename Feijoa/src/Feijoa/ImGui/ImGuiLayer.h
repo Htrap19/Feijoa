a7cdcc8974ba43293251b55d6220ae7d8d38b2c5
Parth <developmentprogrmming154@gmail.com>
2021-09-30 15:04:31 +0200

    Updated to latest commit and added Assimp submodule

--- Feijoa/src/Feijoa/ImGui/ImGuiLayer.h
+++ Feijoa/src/Feijoa/ImGui/ImGuiLayer.h
@@ -20,14 +20,11 @@ namespace Feijoa
 
 		void Begin();
 		void End();
-<<<<<<< HEAD
 
 		void BlockEvents(bool block) { m_BlockEvents = block; }
 
 	private:
 		bool m_BlockEvents = true;
 		float m_Time = 0.0f;
-=======
->>>>>>> eefd98d (Added profiling functions)
 	};
 }
