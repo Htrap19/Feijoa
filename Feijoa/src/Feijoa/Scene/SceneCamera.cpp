50de15b22fd02c297f3d6bb556199cf9c3200d4c
Parth <developmentprogrmming154@gmail.com>
2021-11-26 20:07:18 +0200

    Added Camera Component UI to Properties panel

--- Feijoa/src/Feijoa/Scene/SceneCamera.cpp
+++ Feijoa/src/Feijoa/Scene/SceneCamera.cpp
@@ -13,9 +13,21 @@ namespace Feijoa
 
 	void SceneCamera::SetOthographic(float size, float nearClip, float farClip)
 	{
+		m_ProjectionType = ProjectionType::Orthographic;
+
 		m_OrthographicsSize = size;
 		m_OrthographicNear = nearClip;
 		m_OrthographicFar = farClip;
+
+		RecalculateProjection();
+	}
+
+	void SceneCamera::SetPerspective(float verticalFOV, float nearClip, float farClip)
+	{
+		m_ProjectionType = ProjectionType::Perspective;
+		m_PerspectiveFOV = verticalFOV;
+		m_PerspectiveNear = nearClip;
+		m_PerspectiveFar = farClip;
 
 		RecalculateProjection();
 	}
@@ -28,12 +40,17 @@ namespace Feijoa
 
 	void SceneCamera::RecalculateProjection()
 	{
-		float ortholeft = -m_OrthographicsSize * m_AspectRatio * 0.5f;
-		float orthoright = m_OrthographicsSize * m_AspectRatio * 0.5f;
-		float orthobottom = -m_OrthographicsSize * 0.5f;
-		float orthotop = m_OrthographicsSize * 0.5f;
+		if (m_ProjectionType == ProjectionType::Perspective)
+			m_Projection = glm::perspective(m_PerspectiveFOV, m_AspectRatio, m_PerspectiveNear, m_PerspectiveFar);
+		else
+		{
+			float ortholeft = -m_OrthographicsSize * m_AspectRatio * 0.5f;
+			float orthoright = m_OrthographicsSize * m_AspectRatio * 0.5f;
+			float orthobottom = -m_OrthographicsSize * 0.5f;
+			float orthotop = m_OrthographicsSize * 0.5f;
 
-		m_Projection = glm::ortho(ortholeft, orthoright, orthobottom, orthotop, m_OrthographicNear, m_OrthographicFar);
+			m_Projection = glm::ortho(ortholeft, orthoright, orthobottom, orthotop, m_OrthographicNear, m_OrthographicFar);
+		}
 	}
 
 }
--- Feijoa/src/Feijoa/Scene/SceneCamera.h
+++ Feijoa/src/Feijoa/Scene/SceneCamera.h
@@ -7,22 +7,49 @@ namespace Feijoa
 	class SceneCamera : public Camera
 	{
 	public:
+		enum class ProjectionType { Perspective = 0, Orthographic = 1 };
+	public:
 		SceneCamera();
 		virtual ~SceneCamera() = default;
 
 		void SetOthographic(float size, float nearClip, float farClip);
+		void SetPerspective(float verticalFOV, float nearClip, float farClip);
+
 		void SetViewportSize(uint32_t width, uint32_t height);
+
+		inline float GetPerspectiveVerticalFOV() const { return m_PerspectiveFOV; }
+		inline void SetPerspectiveVerticalFOV(float verticalFOV) { m_PerspectiveFOV = verticalFOV; RecalculateProjection(); }
+
+		inline float GetPerspectiveNearClip() const { return m_PerspectiveNear; }
+		inline void SetPerspectiveNearClip(float nearClip) { m_PerspectiveNear = nearClip; RecalculateProjection(); }
+
+		inline float GetPerspectiveFarClip() const { return m_PerspectiveFar; }
+		inline void SetPerspectiveFarClip(float farClip) { m_PerspectiveFar = farClip; RecalculateProjection(); }
 
 		inline float GetOrthographicSize() const { return m_OrthographicsSize; }
 		inline void SetOrthographicSize(float size) { m_OrthographicsSize = size; RecalculateProjection(); }
+
+		inline float GetOrthographicNearClip() const { return m_OrthographicNear; }
+		inline void SetOrthographicNearClip(float nearClip) { m_OrthographicNear = nearClip; RecalculateProjection(); }
+
+		inline float GetOrthographicFarClip() const { return m_OrthographicFar; }
+		inline void SetOrthographicFarClip(float farClip) { m_OrthographicFar = farClip; RecalculateProjection(); }
+
+		inline ProjectionType GetProjectionType() const { return m_ProjectionType; }
+		inline void SetProjectionType(ProjectionType type) { m_ProjectionType = type; }
 		
 	private:
 		void RecalculateProjection();
 
 	private:
+		ProjectionType m_ProjectionType = ProjectionType::Orthographic;
+
+		float m_PerspectiveFOV = glm::radians(45.0f);
+		float m_PerspectiveNear = 0.01f, m_PerspectiveFar = 1000.0f;
+		
 		float m_OrthographicsSize = 10.0f;
 		float m_OrthographicNear = -1.0f, m_OrthographicFar = 1.0f;
-
+		
 		float m_AspectRatio = 0.0f;
 	};
 }
